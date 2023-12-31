1f74e9ca1f98efe17c7d0a219871e4a4490a0f21
leemadov <143035177+leemadov@users.noreply.github.com>
2023-12-31 23:17:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function updateCountdown() {
-    const countdownDate = new Date("2024-01-01T00:00:00").getTime();
+    const countdownDate = new Date("2023-01-01T00:00:00").getTime();
     const now = new Date().getTime();
     const difference = countdownDate - now;
-  
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
   
     if (difference <= 0) {
       clearInterval(countdownInterval);
       document.body.classList.add("confetti-bg");
+      document.getElementById("timer").innerHTML = "Happy New Year!";
+    } else {
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  
+      document.getElementById("days").innerText = days;
+      document.getElementById("hours").innerText = hours;
+      document.getElementById("minutes").innerText = minutes;
+      document.getElementById("seconds").innerText = seconds;
     }
   }
   
